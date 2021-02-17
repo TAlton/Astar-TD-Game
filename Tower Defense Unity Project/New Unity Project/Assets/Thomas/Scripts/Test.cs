894de99d290a9187fa04f1e1543066f01783c841
Thomas Monarch Alton <23666162+TAlton@users.noreply.github.com>
2021-02-17 15:31:55 +0000

    Implemented clockwise check and triangle separation

--- Tower Defense Unity Project/New Unity Project/Assets/Thomas/Scripts/Test.cs
+++ Tower Defense Unity Project/New Unity Project/Assets/Thomas/Scripts/Test.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     [SerializeField] private Vector3[] m_MeshVertices;
-    [SerializeField] private List<Triangle> m_Triangles;
+    [SerializeField] private List<Triangle> m_Triangles = new List<Triangle>();
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +13,19 @@ public class Test : MonoBehaviour
     }
     private void Awake()
     {
-        m_MeshVertices              = this.GetComponent<MeshFilter>().mesh.vertices;
+        m_MeshVertices = this.GetComponent<MeshFilter>().mesh.vertices;
+
         for (int i = 0; i < m_MeshVertices.Length; i++)
         {
-            m_MeshVertices[i]       = m_MeshVertices[i].normalized;
+            m_MeshVertices[i] = m_MeshVertices[i].normalized;
         }
 
-        int[] lsArrTriangles        = this.GetComponent<MeshFilter>().mesh.triangles;
-        for (int i = 0; i < lsArrTriangles.Length; i += 3)
+        int[] lsArrTriangles = this.GetComponent<MeshFilter>().mesh.triangles;
+
+        for (int i = 0; i < (lsArrTriangles.Length - 3); i += 3)
         {
-            Triangle lsTriangle = new Triangle(m_MeshVertices[i], m_MeshVertices[i + 1], m_MeshVertices[i + 2]);
-            m_Triangles.Add(lsTriangle);
+            m_Triangles.Add(new Triangle(m_MeshVertices[lsArrTriangles[i]], m_MeshVertices[lsArrTriangles[i + 1]], m_MeshVertices[lsArrTriangles[i + 2]]));
+
         }
     }
 
@@ -32,16 +34,31 @@ public class Test : MonoBehaviour
     {
         
     }
-    public static void OrientClockwise(int[] argTriangles) {
-        for(int i = 0; i < argTriangles.Length; i++)
+    public static void OrientClockwise(List<Triangle> argTriangles) {
+        for(int i = 0; i < argTriangles.Count; i++)
         {
-
+            //make all tris clockwise
+            if (!IsClockwise(argTriangles[i])) ChangeOrientation(argTriangles[i]);
         }
     }
+    public static bool IsClockwise(Triangle argTri)
+    {
+        //a curve is clockwise if you get a positive value when going start to finish vice versa given that there is no intersection
+        float lsDeterminant = argTri.v1.x * argTri.v2.y + argTri.v3.x * argTri.v1.y + argTri.v2.x * argTri.v3.y - argTri.v1.x * argTri.v3.y - argTri.v3.x * argTri.v2.y - argTri.v2.x * argTri.v1.y;
+
+        if (lsDeterminant > 0f) return false;
+
+        return true;
+    }
+    public static void ChangeOrientation(Triangle argTriangle)
+    {
+
+    }
 }
-public class Triangle
+
+[System.Serializable] public class Triangle
 {
-    Vector3 v1, v2, v3;
+    [SerializeField] public Vector3 v1, v2, v3;
 
     public Triangle(Vector3 argV1, Vector3 argV2, Vector3 argV3)
     {
